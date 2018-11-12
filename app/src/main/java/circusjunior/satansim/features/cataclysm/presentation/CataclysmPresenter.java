3f43a81ceb963109ae0646e7862e32426b47a274
Олег <invisible6462@gmail.com>
2018-11-13 02:43:03 +0700

    invis
    Test 0.0.1

--- app/src/main/java/circusjunior/satansim/features/cataclysm/presentation/CataclysmPresenter.java
+++ app/src/main/java/circusjunior/satansim/features/cataclysm/presentation/CataclysmPresenter.java
@@ -1,9 +1,11 @@
 package circusjunior.satansim.features.cataclysm.presentation;
 
 
+import circusjunior.satansim.GameThreadManager;
 
 public final class CataclysmPresenter {
+    private GameThreadManager gameTM;
     public void testHard() {
-        
+        gameTM.GameTreadTest();
     }
 }
